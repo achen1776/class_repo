520c59137493b57270a3ac43fbc485ccfb79c655
Alex Chen <alexander.chen@duke.edu>
2022-01-31 16:24:29 -0500

    added in the test blood calculator file

---
+++ test_blood_calculator.py
@@ -0,0 +1,20 @@
+#test_blood_calculator.py
+
+import pytest
+
+from blood_calculator import check_HDL
+
+@pytest.mark.parametrize("input_HDL, expected", [
+    [70, "Normal"],
+    [45, "Borderline Low"],
+    [20, "Low"]
+    ])
+def test_check_HDL(input_HDL, expected):
+    answer = check_HDL(input_HDL)
+    assert answer == expected
+    
+##Look into pep 8 style guide
+    
+#pytest -v --pycodestyle
+
+#pylint
