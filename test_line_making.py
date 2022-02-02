1269520ddbb31a6fee11ea657974f9c73a777c7b
achen1776 <alexander.chen@duke.edu>
2022-02-02 15:34:03 -0500

    uplaoding code

---
+++ test_line_making.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+def line_function(points,x):
+    points=([1,1],[4,4])
+@pytest.mark.parametrize("x", [
+    [1],
+    [2],
+    [3]
+    ])
+def test_line_function(x):
+    print(hello)
